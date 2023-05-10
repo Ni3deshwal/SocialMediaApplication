b8017b4a0dbd778239e33364bacee1e4a5d9776d
Ni3deshwal <ni3deswal@gmail.com>
2023-05-10 23:28:15 +0530

    server side part is completed

--- front-end/server/controllers/post.controller.js
+++ front-end/server/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import userModel from '../models/user.model.js'
 import postModel from "../models/post.model.js";
 import mongoose from "mongoose";
 
@@ -22,5 +23,97 @@ export const getPost = async (req, res) => {
         res.status(200).json(post);
     } catch (error) {
         res.status(500).json(error)
+    }
+}
+//update post
+export const updatePost=async(req,res)=>{
+    const postId = req.params.id;
+    const {userId}=req.body;
+    try {
+        const post=await postModel.findById(postId);
+        if(post.userId===userId)
+        {
+            await post.updateOne({$set:req.body});
+            res.status(200).json("post updated")
+        }
+        else{
+            res.status(403).json("Action Forbidden");
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+//delete a post
+export const deletePost=async(req,res)=>{
+    const id=req.params.id;
+    const{userId}=req.body;
+    try {
+        const post=await postModel.findById(id);
+        if(post.userId==userId){
+            await post.deleteOne();
+            res.status(200).json("post deleted successfully")
+        }
+    } catch (error) {
+        res.status(500).json(error)
+        
+    }
+}
+//like and dislike post
+
+export const likePost=async(req,res)=>{
+        const id=req.params.id;
+        const{userId}=req.body;
+        try {
+            const post=await postModel.findById(id);
+            if(!post.likes.includes(userId)){
+                await post.updateOne({$push:{likes:userId}});
+                res.status(200).json("post liked successfully")
+            }
+            else{
+                await post.updateOne({$pull:{likes:userId}});
+                res.status(200).json("post unliked successfully")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+            
+        }
+}
+//gettimelinePost
+
+export const gettimelinePost=async(req,res)=>{
+    const userId=req.params.id;
+
+    try {
+        const currentUserPost=await postModel.find({userId:userId})
+        const followingPost=await userModel.aggregate([
+            {
+            
+                $match:
+                {
+                    _id:new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup:
+                {
+                    from:"posts",
+                    localField:"following",
+                    foreignField:userId,
+                    as:"followingPost"
+                }
+            },
+            {
+                $project:{
+                    followingPost:1,
+                    _id:0
+                }
+            }
+    ])
+    res.status(200).json(currentUserPost.concat(...followingPost[0].followingPost).sort((a,b)=>{
+        return b.createdAt-a.createdAt;
+    }))
+    
+    } catch (error) {
+        res.status(403).json(error)
     }
 }
--- front-end/server/routes/postroutes.js
+++ front-end/server/routes/postroutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { createPost, getPost } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPost, gettimelinePost, likePost, updatePost } from '../controllers/post.controller.js';
 const router=express.Router();
 
 router.post('/',createPost)
 router.get('/:id',getPost);
+router.put('/:id',updatePost);
+router.delete('/:id',deletePost)
+router.put('/:id/like',likePost)
+router.get('/:id/timeline',gettimelinePost)
 
 export default router
