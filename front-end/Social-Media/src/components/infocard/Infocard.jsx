2537093c49f30b13c78ed36ef1b5649628996b77
Ni3deshwal <ni3deswal@gmail.com>
2023-05-20 21:47:19 +0530

    upadate

--- front-end/Social-Media/src/components/infocard/Infocard.jsx
+++ front-end/Social-Media/src/components/infocard/Infocard.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ic from '../infocard/Infocard.module.css'
 import {UilPen} from'@iconscout/react-unicons'
 import Profilemodal from '../profilemodal/Profilemodal.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 function Infocard() {
     const[modalOpened,setModalOpened]=useState(false);
-    
+    const dispatch=useDispatch();
+    const param=useParams();
+    const profileuserid=params.id
+    const[profileuser,setProfileuser]=useState({})
+    const {user}=useSelector((state)=state.AuthReducer.authdata)
+    useEffect(()=>{
+        const fetchprofileuser=async(()=>{
+            if(profileuserid=user._id)
+            {
+                setProfileuser(user)
+            }
+            else{
+                
+            }
+        })
+    })
   return (
     
     <div className={ic.infocard}>
